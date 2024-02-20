349b78753b1006ae658869c291975984036104da
Matheus Gomes Rocha <matheusxvtb@poli.ufrj.br>
2024-02-20 16:49:44 -0300

    Update

--- Esp8266/src/main.cpp
+++ Esp8266/src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  pinMode(BUILTIN_LED, OUTPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(BUILTIN_LED, LOW);
+
+  delay(100);
+
+  digitalWrite(BUILTIN_LED, HIGH);
+
+  delay(100); 
+
 }
