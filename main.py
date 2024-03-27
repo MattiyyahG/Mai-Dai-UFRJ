78c33ee09d15fe187454375c8ddd66a5d719c53c
Matheus Gomes Rocha <81640187+MattiyyahG@users.noreply.github.com>
2024-03-27 10:48:43 -0300

    Update main.py
    atualização main.
    resolvendo problemas de compatibilidade

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI
 
 from fastapi_mqtt import FastMQTT, MQTTConfig
 
-from routes.routes import endPoints
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -18,11 +17,10 @@ client = AsyncIOMotorClient(MONGO_URL)
 
 database = client["MaiDai"]
 
-collection = database["c_Uva"]
+collection = database["uva"]
 
 app = FastAPI()
 
-app.include_router(endPoints)
 
 mqtt_config = MQTTConfig(
     host = "test.mosquitto.org",
@@ -39,11 +37,6 @@ mqtt = FastMQTT(
 
 mqtt.init_app(app)
 
-@endPoints.get("/")
-def home():
-    return{
-
-    }
 
 @mqtt.on_connect()
 def connect(client, flags, rc , properties):
@@ -57,23 +50,25 @@ async def message(client, topic, payload, qos, properties):
 
     data = payload.decode(encoding='UTF-8')
 
-    collection.insert_one({"topic": topic, "Umidade(%)" : data})
+    collection.insert_one({"topic": topic, "Umidade(%)" : data[:5], "ID": data[6]})
 
-    print(f"Mensagem recebida: ", data)
+    print(f"Mensagem recebida: ", data, "id: ", data[6])
+
+    return 0
 
 
 
 @mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
-    print("Desconectado", packet , exc)
+    print("Desconectado")
 
 @mqtt.on_subscribe()
 def subscribe(client, mid, qos, properties):
     print("Inscrito", client, mid, qos , properties)
   
 @mqtt.subscribe("MaiDai/Uva")
-#async def message_to_topic(client, topic, payload, qos, properties):
-    #'''print("Mensagem recebida no tópico: ", topic, payload.decode(), qos, properties)'''
+async def message_to_topic(client, topic, payload, qos, properties):
+    '''print("Mensagem recebida no tópico: ", topic, payload.decode(), qos, properties)'''
 
 @app.get("/")
 async def func():
